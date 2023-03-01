49e54c474e9c32f60ed4b2aa74f5fa496f9c69e2
AllisonLin8 <doging8@gmail.com>
2023-03-01 15:12:39 +0800

    Add comments and modify scripts

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -11,10 +11,10 @@ router.get('/', (req, res) => {
 // 提交要縮短的網址
 router.post('/', async (req, res) => {
     try {
-        const result = await Url.findOne({ originUrl: req.body.originUrl })
-        if (result) {
+        const result = await Url.findOne({ originUrl: req.body.originUrl }) // 檢查資料庫有沒有這筆原網址
+        if (result) { // 有，返回已經建立過的短網址
             return res.render('generate', { shortUrl: result.shortUrl })
-        } else {
+        } else { // 無，生成新的短網址且檢查有無和已有的短網址重複，再返回建立完成的資料
             const newData = await checkShortUrl(generateShortUrl(req.body.originUrl, 5))
             Url.create(newData)
             return res.render('generate', { shortUrl: newData.shortUrl })
